93702e5b80494e558ef4708d5621a9b454213c53
sevaradeveloper <sevara123@icloud.com>
2025-09-28 20:37:06 +0300

    ani qoshtim

--- js/main.js
+++ js/main.js
@@ -39,19 +39,19 @@
     const navLinks = document.getElementById('navLinks');
     const mobileCloseBtn = document.getElementById('mobileCloseBtn');
     
-    // Открытие меню по клику на гамбургер
+    
     hamburger.addEventListener('click', function() {
         hamburger.classList.toggle('active');
         navLinks.classList.toggle('active');
     });
     
-    // Закрытие меню по клику на кнопку X
+ 
     mobileCloseBtn.addEventListener('click', function() {
         hamburger.classList.remove('active');
         navLinks.classList.remove('active');
     });
     
-    // Закрытие меню по клику на ссылку (опционально)
+   
     const navItems = navLinks.querySelectorAll('a');
     navItems.forEach(item => {
         item.addEventListener('click', function() {
@@ -88,17 +88,17 @@ document.querySelectorAll('.goods__item').forEach(item => {
   const priceText = item.querySelector('.goods__sidebar-text.price');
   const image = item.querySelector('.goods__image');
 
-  // Функция для скрытия всех боковых панелей
+ 
   function hideAllSidebars() {
     document.querySelectorAll('.goods__sidebar.active').forEach(activeSidebar => {
       activeSidebar.classList.remove('active');
     });
   }
 
-  // Клик по цветам
+ 
   colors.forEach(color => {
     color.addEventListener('click', (e) => {
-      // Сначала скрываем все открытые панели
+      
       hideAllSidebars();
       
       colors.forEach(c => c.classList.remove('active'));
@@ -110,22 +110,21 @@ document.querySelectorAll('.goods__item').forEach(item => {
     });
   });
 
-  // Клик по картинке
+ 
   image.addEventListener('click', () => {
-    // Сначала скрываем все открытые панели
+    
     hideAllSidebars();
     sidebar.classList.add('active');
   });
 });
 
-// Обработчик клика по документу для закрытия боковой панели
 document.addEventListener('click', (e) => {
-  // Если клик был не по боковой панели, не по цветам, не по изображению и не по плюсу
+  
   if (!e.target.closest('.goods__sidebar') && 
       !e.target.closest('.goods__color') && 
       !e.target.closest('.goods__image') &&
       !e.target.closest('.goods__plus')) {
-    // Скрываем все активные боковые панели
+    
     document.querySelectorAll('.goods__sidebar.active').forEach(sidebar => {
       sidebar.classList.remove('active');
     });
