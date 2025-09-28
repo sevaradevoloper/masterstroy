19d53fc7f6aaf5b27699c09f0fb50725b42f9a1c
sevaradeveloper <sevara123@icloud.com>
2025-09-28 10:47:16 +0300

    qolgani

--- js/main.js
+++ js/main.js
@@ -10,3 +10,36 @@
     const elem = document.getElementById(id);
     elem.style.display = (elem.style.display === "block") ? "none" : "block";
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.getElementById('hamburger-menu');
+    const navLinks = document.querySelector('.nav__links');
+    
+    hamburger.addEventListener('click', function() {
+        hamburger.classList.toggle('active');
+        navLinks.classList.toggle('active');
+    });
+});
